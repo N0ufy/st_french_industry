6941d5aab15b16ec8c99f39071a816a2db438d77
Vivien Renouf <vivierenouf@hotmail.com>
2023-04-02 01:22:15 +0200

    Update st_french_industry.py

--- st_french_industry.py
+++ st_french_industry.py
@@ -27,7 +27,7 @@ import numpy as np
 import pydeck as pdk
 
 df = pd.read_csv('merged_NO_NA_geo_salary_ets.csv')
-df['elevation'] = df['E14TST ']
+df['elevation'] = df['E14TST']
 df_quantile1 = df[df['salaire_categorie'] == 'quantile 1'].reset_index(drop=True)
 df_quantile2 = df[df['salaire_categorie'] == 'quantile 2'].reset_index(drop=True)
 df_quantile3 = df[df['salaire_categorie'] == 'quantile 3'].reset_index(drop=True)
