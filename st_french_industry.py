b4eb500e2bee2dd0638303e7d7c1b06abea816ae
Vivien Renouf <vivierenouf@hotmail.com>
2023-04-01 22:41:36 +0200

    Update st_french_industry.py

--- st_french_industry.py
+++ st_french_industry.py
@@ -7,17 +7,9 @@ st.title("""
 # French **Industry** #
 # """)
 
-col1, col2, col3 = st.columns(3, gap='large')
-
-with col1:
     st.header('Test 1')
     latitude = [i for i in df.latitude]
     longitude = [i for i in df.longitude]
     df_geo = pd.DataFrame({ 'lat': latitude , 'lon': longitude})
 
     st.map(df_geo, use_container_width=True)
-with col2:
-    st.header('Test 2')
-
-with col3:
-    st.header('Test 3')
