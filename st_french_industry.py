053b44a8021341b061785a738216006dca6b803a
Vivien Renouf <vivierenouf@hotmail.com>
2023-04-02 00:42:26 +0200

    Update st_french_industry.py

--- st_french_industry.py
+++ st_french_industry.py
@@ -1,3 +1,4 @@
+"""
 import pandas as pd
 
 
@@ -14,7 +15,46 @@ st.title("""
 
 #col1, col2, col3, col4 = st.columns(4)
 
+"""
 st.header('Bas salaires - 1er quantile')
 st.map(df_quantile1 , use_container_width=True)
 st.header('Classe moyenne inf - 2ème quantile')
 st.map(df_quantile2 , use_container_width=True)
+"""
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pydeck as pdk
+
+chart_data = pd.DataFrame(
+   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+   columns=['lat', 'lon'])
+
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=37.76,
+        longitude=-122.4,
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=chart_data,
+           get_position='[lon, lat]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
