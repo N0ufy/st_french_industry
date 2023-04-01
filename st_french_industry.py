b0c341b05d58d4a39918827271e69405713dcfb9
Vivien Renouf <vivierenouf@hotmail.com>
2023-04-02 01:01:40 +0200

    Update st_french_industry.py

--- st_french_industry.py
+++ st_french_industry.py
@@ -27,8 +27,10 @@ import numpy as np
 import pydeck as pdk
 
 df = pd.read_csv('merged_NO_NA_geo_salary_ets.csv')
-lat = [i for i in df.latitude]
-lon = [i for i in df.longitude]
+df_quantile1 = df[df['salaire_categorie'] == 'quantile 1'].reset_index(drop=True)
+df_quantile2 = df[df['salaire_categorie'] == 'quantile 2'].reset_index(drop=True)
+df_quantile3 = df[df['salaire_categorie'] == 'quantile 3'].reset_index(drop=True)
+df_quantile4 = df[df['salaire_categorie'] == 'quantile 4'].reset_index(drop=True)
 
 chart_data = pd.DataFrame({'lat': lat, 'lon':lon})
 
@@ -43,19 +45,19 @@ st.pydeck_chart(pdk.Deck(
     layers=[
         pdk.Layer(
            'HexagonLayer',
-           data=chart_data,
-           get_position='[lon, lat]',
+           data=df_quantile4,
+           get_position='[longitude, latitude]',
            radius=200,
            elevation_scale=4,
-           elevation_range=[0, 250],
+           elevation_range=[0, 1000],
            pickable=True,
            extruded=True,
         ),
 
          pdk.Layer(
            'HexagonLayer',
-           data=chart_data,
-           get_position='[lon, lat]',
+           data=df_quantile3,
+           get_position='[longitude, latitude]',
            radius=200,
            elevation_scale=4,
            elevation_range=[250, 500],
@@ -64,7 +66,17 @@ st.pydeck_chart(pdk.Deck(
         ),
          pdk.Layer(
            'HexagonLayer',
-           data=chart_data,
+           data=df_quantile2,
+           get_position='[lon, lat]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[500, 700],
+           pickable=True,
+           extruded=True,
+        ),
+         pdk.Layer(
+           'HexagonLayer',
+           data=df_quantile1,
            get_position='[lon, lat]',
            radius=200,
            elevation_scale=4,
