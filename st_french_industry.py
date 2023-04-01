dfd1c90691ced73ad48e6e755639a38f296a4f76
Vivien Renouf <vivierenouf@hotmail.com>
2023-04-01 22:29:27 +0200

    Update st_french_industry.py

--- st_french_industry.py
+++ st_french_industry.py
@@ -4,5 +4,16 @@ import streamlit as st
 df = pd.read_csv('merged_NO_NA_geo_salary_ets.csv')
 
 st.title("""
-# French **Industry**
+# French **Industry** #
 # """)
+
+col1, col2, col3 = st.columns(3, gap='large')
+
+with col1:
+    st.header('Test 1')
+
+with col2:
+    st.header('Test 2')
+
+with col3:
+    st.header('Test 3')
