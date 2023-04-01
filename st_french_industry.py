700da7d0eee41c1e4b758a918e03680bb5c4c0c9
Vivien Renouf <vivierenouf@hotmail.com>
2023-04-02 00:30:31 +0200

    added salary cat

--- st_french_industry.py
+++ st_french_industry.py
@@ -1,15 +1,17 @@
 import pandas as pd
-import streamlit as st
+
 
 df = pd.read_csv('merged_NO_NA_geo_salary_ets.csv')
+df_quantile1 = df[df['salaire_categorie'] == 'quantile 1'].reset_index(drop=True)
+df_quantile2 = df[df['salaire_categorie'] == 'quantile 2'].reset_index(drop=True)
+df_quantile3 = df[df['salaire_categorie'] == 'quantile 3'].reset_index(drop=True)
+df_quantile4 = df[df['salaire_categorie'] == 'quantile 4'].reset_index(drop=True)
 
+import streamlit as st
 st.title("""
 #French **Industry**#
 # """)
 
-city = [i for i in df.LIBGEO]
-latitude = [i for i in df.latitude]
-longitude = [i for i in df.longitude]
-df_geo = pd.DataFrame({ 'city': city, 'lat': latitude , 'lon': longitude})
+#df_geo = pd.DataFrame({'lat': latitude , 'lon': longitude})
 
-st.map(df_geo, use_container_width=True)
+st.map(df_quantile1 , use_container_width=True)
