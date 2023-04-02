104075337b52bbf04b8c9f8045d6ee45a507c3c0
Vivien Renouf <vivierenouf@hotmail.com>
2023-04-02 22:13:36 +0200

    Update st_french_industry.py

--- st_french_industry.py
+++ st_french_industry.py
@@ -26,7 +26,7 @@ st.pydeck_chart(pdk.Deck(
            auto_highlight=True,
            #elevation_scale=5,
            pickable=True,
-           #elevation_range= [50,50],  
+           elevation_range= [0,1000],  
            extruded=True,
            coverage=1,
            radius=400,
